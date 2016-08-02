17090a84d64d6c9abf3ad5767ca55e6ed4dacf7e
Doug <doug.macnerland@gmail.com>
2016-08-02 08:38:48 -0500

    Scan for devices

--- app/src/main/java/me/macnerland/bluetooth/MainActivity.java
+++ app/src/main/java/me/macnerland/bluetooth/MainActivity.java
@@ -3,6 +3,9 @@ package me.macnerland.bluetooth;
 import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
@@ -26,20 +29,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.tab_pager);
 
         adapter = new mPagerAdapter(this.getSupportFragmentManager(), getResources());
-        ViewPager vp = (ViewPager)findViewById(R.id.pager);
+        ViewPager vp = (ViewPager) findViewById(R.id.pager);
         vp.setAdapter(adapter);
 
 
         BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
         //if bluetooth is disabled, launch an intent to enable it.
-        if(bluetoothAdapter == null || !bluetoothAdapter.isEnabled()){
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+        BluetoothLeScanner BLEscanner = bluetoothAdapter.getBluetoothLeScanner();
 
-
-
-
+        BLEscanner.startScan(new ScanCallback() {
+            @Override
+            public void onScanResult(int callbackType, ScanResult result) {
+                super.onScanResult(callbackType, result);
+                Log.i(TAG, result.toString());
+            }
+        });
     }
 }
