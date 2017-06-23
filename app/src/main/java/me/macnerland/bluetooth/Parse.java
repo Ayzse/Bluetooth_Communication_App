0000f2f9d7ba15495bb7a178d85a0c02f3a0dd79
Ayzse <macnerl2@illinois.edu>
2017-06-23 17:32:25 -0500

    Communcatoin to multiple devices

--- app/src/main/java/me/macnerland/bluetooth/Parse.java
+++ app/src/main/java/me/macnerland/bluetooth/Parse.java
@@ -2,8 +2,10 @@ package me.macnerland.bluetooth;
 
 import android.util.Log;
 
+import java.io.BufferedOutputStream;
 import java.io.IOError;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,12 +19,23 @@ class Parse {
 
     private static final String TAG = "parse";
 
-    static void create_user(){
+    static void create_user(String username, String password){
+        String post_body = "{\"username\":\""+username+"\", \"password\":\""+password+"\" }";
+
+
         try {
             URL create_user_endpoint = new URL("https", "grainportalv2.herokuapp.com", "parse/users");
             try {
                 HttpURLConnection connection = (HttpURLConnection) create_user_endpoint.openConnection();
+                try{
+                    connection.getDoOutput();
+                    connection.setChunkedStreamingMode(0);
 
+                    OutputStream out = new BufferedOutputStream(connection.getOutputStream());
+
+                } finally {
+                    connection.disconnect();
+                }
             }   catch(IOException e){
                 Log.e(TAG, "IO Exception" + e.toString());
             }
